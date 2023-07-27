cd0795d5e70ca9eb589456e91ea272563dc2ac9b
tharyck gusmao <thaka.gus@gmail.com>
2023-07-26 23:49:25 -0300

    fix commands cli npm-utils

--- studies/exercicies/00-npm-utils/src/api/commands.rs
+++ studies/exercicies/00-npm-utils/src/api/commands.rs
@@ -1,59 +1,55 @@
-use std::{process::{self, Command}, fmt::format};
+use std::{
+    fmt::format,
+    process::{self, Command},
+};
 
-pub fn rollup(input: &String){
-    let command = format!(
-            "{} {} {}",
-            "yarn workspace ", input, " run rollup"
-        );
-        let output = Command::new(command).output();
-
-        match output {
-            Ok(result) => {
-                println!("status: {}", result.status);
-                println!("stdout: {}", String::from_utf8_lossy(&result.stdout));
-                println!("stderr: {}", String::from_utf8_lossy(&result.stderr));
-                publish(&input)
-            }
-            Err(e) => {
-                eprintln!("Application error: {}", e);
-                process::exit(1);
-            }
-        }
-}
-pub fn publish(input: &String){
-    let command = format!(
-            "{} {} {}",
-            "yarn workspace ", input, " run publish"
-        );
-        let output = Command::new(command).output();
-
-        match output {
-            Ok(result) => {
-                println!("status: {}", result.status);
-                println!("stdout: {}", String::from_utf8_lossy(&result.stdout));
-                println!("stderr: {}", String::from_utf8_lossy(&result.stderr));
-            }
-            Err(e) => {
-                eprintln!("Application error: {}", e);
-                process::exit(1);
-            }
-        }
-}
-
-pub fn unpublish(input: &String){
-   
-    let output = Command::new("npm")
-    .args(["publish",input])
-    .arg("--registry http://localhost:4873/")
-    .arg("--force")
-    .spawn();
+pub fn rollup(input: &String) {
+    let output = Command::new("bash")
+        .arg("-c")
+        .arg(format!("yarn workspace {} run rollup", input))
+        .output();
 
     match output {
         Ok(result) => {
+            println!("status: {:?}", String::from_utf8_lossy(&result.stdout));
+            publish(&input);
+        }
+        Err(e) => {
+            eprintln!("Application error: {}", e);
+            process::exit(1);
+        }
+    }
+}
+pub fn publish(input: &String) {
+    let output = Command::new("bash")
+        .arg("-c")
+        .arg(format!("yarn workspace {} run publish", input))
+        .output();
 
-            println!("status: {:?}", &result.stdin.unwrap());
-            println!("stdout: {:?}", &result.stdout.unwrap());
-            println!("stderr: {:?}", &result.stderr.unwrap());
+    match output {
+        Ok(result) => {
+            println!("status: {:?}", String::from_utf8_lossy(&result.stdout));
+            process::exit(1);
+        }
+        Err(e) => {
+            eprintln!("Application error: {}", e);
+            process::exit(1);
+        }
+    }
+}
+
+pub fn unpublish(input: &String) {
+    let output = Command::new("bash")
+        .arg("-c")
+        .arg(format!(
+            "npm unpublish --registry http://localhost:4873/ --force {}",
+            input
+        ))
+        .output();
+
+    match output {
+        Ok(result) => {
+            println!("status: {:?}", String::from_utf8_lossy(&result.stdout));
             rollup(&input);
         }
         Err(e) => {
@@ -64,8 +60,7 @@ pub fn unpublish(input: &String){
 }
 
 pub fn pack(input: &String) -> String {
-    
     unpublish(input);
 
-    return String::from("DONE")
+    return String::from("DONE");
 }
